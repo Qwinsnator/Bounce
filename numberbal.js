fc92b7c7b5d6487191e7c3049010708028cc8235
Qwinsnator <quintenman4@gmail.com>
2021-09-19 15:11:49 +0000

    e

---
+++ numberbal.js
@@ -0,0 +1,21 @@
+  class nummer extends Bal {
+  nummer;
+
+  constructor(newX, newY, _speedX, _speedY, _nummer) {
+    super(_x, _y, _speedX, _speedY)
+    this.nummer = _nummer;
+  }
+
+  // toont de bal
+  show() {
+    fill(0,0,0);
+    ellipse(this.x, this.y, 50, 50);
+    fill(255,255,255);
+    ellipse(this.x, this.y, 45, 45);
+    textSize(25);
+    fill(0,0,0);
+    text("8",this.x-5, this.y+5);
+  }
+    
+
+}
